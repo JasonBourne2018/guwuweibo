9e965ad21b6cb8c98cdb2add8be41048ac4ae2de
Jason Bourne <1508366362@qq.com>
2019-12-02 07:22:11 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -35,4 +35,27 @@ class UsersController extends Controller
         session()->flash('success', 'welcome, you gonna begin a new journey from here');
         return redirect()->route('users.show', [$user]);
     }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name'  =>  'required|max:50',
+            'password'  =>  'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['passowrd'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '用户资料编辑成功');
+        return redirect()->route('users.show', compact('user'));
+    }
 }
